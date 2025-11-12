a7d9528fb17e7236bf0058651260fe7a9047ebf9
Mend1 User <mendiarkarzhaubai1@gmail.com>
2025-11-13 01:25:26 +0500

    Downloaded TailwinCss and Axios for dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+<div>
+  <h1 class="text-3xl text-red font-bold underline">Hello World!</h1>
+</div>
 </template>
 
-<style scoped></style>
